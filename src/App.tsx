8b5e707e520a611d4edcd25a6be6a1709841b8ca
Harvindsokhal <harvindsokhal@gmail.com>
2024-01-06 00:31:03 +0000

    wifi signal test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Topbar, Taskbar } from './components'
+import './App.scss'
 
 const App = () => {
   return (
-    <div>
+    <div className='container'>
       <Topbar />
+      <div id='router'>
+        <div id='body'>
+          <div id='wifi'>
+            <div className='signal' />
+            <div className='signal' />
+            <div className='signal' />
+          </div>
+        </div>
+      </div>
       <Taskbar />
     </div>
   )
