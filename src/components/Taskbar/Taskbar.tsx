47c865cfeffa3c063f14ce3f322db09b1c9556df
Harvindsokhal <harvindsokhal@gmail.com>
2024-01-06 00:10:03 +0000

    Taskbar transition

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -5,13 +5,13 @@ const Taskbar = () => {
   return (
     <div className='task-bar'>
       <div className='task-bar-items'>
-        <img src={assets.messageIcon} alt='message-icon' />
-        <img src={assets.newsIcon} alt='news-icon' />
-        <img src={assets.appStoreIcon} alt='appstore-icon' />
-        <img src={assets.notesIcon} alt='notes-icon' />
-        <img src={assets.safariIcon} alt='safari-icon' />
-        <img src={assets.folderIcon} alt='folder-icon' />
-        <img src={assets.binIcon} alt='bin-icon' />
+        <input type='image' src={assets.messageIcon} alt='message-icon' />
+        <input type='image' src={assets.newsIcon} alt='news-icon' />
+        <input type='image' src={assets.appStoreIcon} alt='appstore-icon' />
+        <input type='image' src={assets.notesIcon} alt='notes-icon' />
+        <input type='image' src={assets.safariIcon} alt='safari-icon' />
+        <input type='image' src={assets.folderIcon} alt='folder-icon' />
+        <input type='image' src={assets.binIcon} alt='bin-icon' />
       </div>
     </div>
   )
