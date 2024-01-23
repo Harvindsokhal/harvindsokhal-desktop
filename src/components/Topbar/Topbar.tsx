91b3264bf052725940825f93a61c0ceccf78d981
Harvindsokhal <harvindsokhal@gmail.com>
2024-01-23 18:08:55 +0000

    Topbar changes

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from 'react'
-import './Topbar.scss'
-import assets from '../../assets'
-import Wifi from './Wifi'
+import { useState, useEffect } from "react";
+import "./Topbar.scss";
+import assets from "../../assets";
+import Wifi from "./Wifi";
 
 const Topbar = () => {
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date());
 
-  const weekday = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
+  const weekday = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"];
 
   useEffect(() => {
-    setInterval(() => setDate(new Date()), 1000)
-  }, [])
+    setInterval(() => setDate(new Date()), 1000);
+  }, []);
 
   return (
-    <div className='Top-bar'>
-      <div className='Top-items-left'>
-        <img src={assets.appleLogo} alt='AppleLogo' className='apple-icon' />
-        <p className='Finder'>Finder</p>
+    <div className="Top-bar">
+      <div className="Top-items-left">
+        <img src={assets.appleLogo} alt="AppleLogo" className="apple-icon" />
+        <p className="Finder">Finder</p>
         <p>File</p>
         <p>Edit</p>
         <p>View</p>
@@ -25,18 +26,22 @@ const Topbar = () => {
         <p>Help</p>
       </div>
 
-      <div className='Top-items-right'>
+      <div className="Top-items-right">
         <Wifi />
-        <img src={assets.airplayIcon} alt='airplay-logo' />
-        <img src={assets.batteryIcon} alt='battery-logo' />
-        <p className='date'>{weekday[date.getDay()]}</p>
-        <p className='date'>
-          {date.getHours()}:{date.getMinutes()}
+        <img src={assets.airplayIcon} alt="airplay-logo" />
+
+        <img src={assets.batteryIcon} alt="battery-logo" />
+        <img src={assets.searchIcon} alt="search-logo" />
+        <p>{weekday[date.getDay()]}</p>
+        <p>{date.getDate()}</p>
+        <p>{month[date.getMonth()]}</p>
+        <p className="time">
+          {date.getHours()}:
+          {date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}
         </p>
-        <img src={assets.searchIcon} alt='search-logo' />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
