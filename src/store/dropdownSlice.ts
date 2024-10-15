0a7d02d6ea481c2efbe75b195faadf3821ea8562
Harvindsokhal <harvindsokhal@gmail.com>
2024-10-15 17:45:51 +0100

    Redux setup

---
+++ src/store/dropdownSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface DropwdownState {
+  section: string | null;
+}
+
+const initialState: DropwdownState = {
+  section: null,
+};
+
+const dropdownSlice = createSlice({
+  name: "dropdown",
+  initialState,
+  reducers: {
+    setSection: (state, action: PayloadAction<string | null>) => {
+      state.section = action.payload;
+    },
+  },
+});
+
+export const { setSection } = dropdownSlice.actions;
+export default dropdownSlice.reducer;
