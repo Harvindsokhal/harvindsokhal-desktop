775fdbb34f798e9deb0ddcaf04f5ca6af001cb45
Harvindsokhal <harvindsokhal@gmail.com>
2024-10-21 18:46:47 +0100

    Top bar dropdown menus

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,7 +1,31 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store'
+import getDropdownContent from '../../utils/getDropdownContent'
 import './Dropdown.scss'
 
 const Dropdown = () => {
-  return <div className="drop-container">Dropdown</div>
+  const dropdownState = useSelector((state: RootState) => state.dropdown)
+  const content = getDropdownContent(dropdownState)
+
+  return (
+    <div className="dropdown-menu">
+      {content.map((item, i) =>
+        item.name === 'divider' ? (
+          <div key={i} className="divider" />
+        ) : (
+          <div
+            key={i}
+            className={`dropdown-item ${item.available ? 'selectable' : 'unselectable'}`}
+            style={{
+              color: item.available ? 'white' : 'rgba(255, 255, 255, 0.55)',
+            }}
+          >
+            {item.name}
+          </div>
+        )
+      )}
+    </div>
+  )
 }
 
 export default Dropdown
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,18 +1,28 @@
 import { useState, useEffect, useRef, useLayoutEffect } from 'react'
 import { assets } from '../../assets'
+import Dropdown from '../Dropdown/Dropdown'
 import Wifi from './Wifi'
 import './Topbar.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../store'
+import { setSection } from '../../store/dropdownSlice'
 
 const Topbar = () => {
   const [date, setDate] = useState(new Date())
-  const [isDropdown, setIsDropdown] = useState(false)
   const [dropdownPosition, setDropdownPosition] = useState<{
     left: number
     top: number
   } | null>(null)
 
+  const [activeTriggerRef, setActiveTriggerRef] =
+    useState<HTMLParagraphElement | null>(null)
+
   const dropdownRef = useRef<HTMLDivElement>(null)
-  const dropdownTriggerRef = useRef<HTMLParagraphElement>(null)
+
+  const dispatch = useDispatch()
+  const dropdownSection = useSelector(
+    (state: RootState) => state.dropdown.section
+  )
 
   const weekday = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
   const month = [
@@ -36,22 +46,21 @@ const Topbar = () => {
   }, [])
 
   useLayoutEffect(() => {
-    if (isDropdown && dropdownTriggerRef.current) {
-      const { left, top, height } =
-        dropdownTriggerRef.current.getBoundingClientRect()
+    if (dropdownSection && activeTriggerRef) {
+      const { left, top, height } = activeTriggerRef.getBoundingClientRect()
       setDropdownPosition({ left, top: top + height })
     }
-  }, [isDropdown])
+  }, [dropdownSection, activeTriggerRef])
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
       if (
         dropdownRef.current &&
         !dropdownRef.current.contains(e.target as Node) &&
-        dropdownTriggerRef.current &&
-        !dropdownTriggerRef.current.contains(e.target as Node)
+        activeTriggerRef &&
+        !activeTriggerRef.contains(e.target as Node)
       ) {
-        setIsDropdown(false)
+        dispatch(setSection(null))
       }
     }
 
@@ -59,62 +68,95 @@ const Topbar = () => {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside)
     }
-  }, [])
+  }, [dispatch, activeTriggerRef])
 
-  const showDropdown = (e: React.MouseEvent<HTMLParagraphElement>) => {
-    e.stopPropagation()
-    setIsDropdown(!isDropdown)
+  const handleMenuClick = (
+    section: string | null,
+    ref: React.RefObject<HTMLParagraphElement>
+  ) => {
+    dispatch(setSection(section))
+    setActiveTriggerRef(ref.current)
   }
+
+  const logoRef = useRef<HTMLImageElement>(null)
+  const finderRef = useRef<HTMLParagraphElement>(null)
+  const fileRef = useRef<HTMLParagraphElement>(null)
+  const editRef = useRef<HTMLParagraphElement>(null)
+  const viewRef = useRef<HTMLParagraphElement>(null)
+  const goRef = useRef<HTMLParagraphElement>(null)
+  const windowRef = useRef<HTMLParagraphElement>(null)
+  const helpRef = useRef<HTMLParagraphElement>(null)
 
   return (
     <div className="Top-bar">
       <div className="Top-items-left">
-        <img src={assets.appleLogo} alt="AppleLogo" className="apple-icon" />
-        <p className="Finder">Finder</p>
+        <img
+          src={assets.appleLogo}
+          alt="AppleLogo"
+          className={`apple-icon ${dropdownSection === 'logo' ? 'active' : ''} `}
+          onClick={() => handleMenuClick('logo', logoRef)}
+          ref={logoRef}
+        />
         <p
-          className={isDropdown ? 'active' : ''}
-          onClick={showDropdown}
-          ref={dropdownTriggerRef}
+          className={`Finder ${dropdownSection === 'finder' ? 'active' : ''}`}
+          onClick={() => handleMenuClick('finder', finderRef)}
+          ref={finderRef}
+        >
+          Finder
+        </p>
+        <p
+          className={dropdownSection === 'file' ? 'active' : ''}
+          onClick={() => handleMenuClick('file', fileRef)}
+          ref={fileRef}
         >
           File
         </p>
-        <p>Edit</p>
-        <p>View</p>
-        <p>Go</p>
-        <p>Window</p>
-        <p>Help</p>
+        <p
+          className={dropdownSection === 'edit' ? 'active' : ''}
+          onClick={() => handleMenuClick('edit', editRef)}
+          ref={editRef}
+        >
+          Edit
+        </p>
+        <p
+          className={dropdownSection === 'view' ? 'active' : ''}
+          onClick={() => handleMenuClick('view', viewRef)}
+          ref={viewRef}
+        >
+          View
+        </p>
+        <p
+          className={dropdownSection === 'go' ? 'active' : ''}
+          onClick={() => handleMenuClick('go', goRef)}
+          ref={goRef}
+        >
+          Go
+        </p>
+        <p
+          className={dropdownSection === 'window' ? 'active' : ''}
+          onClick={() => handleMenuClick('window', windowRef)}
+          ref={windowRef}
+        >
+          Window
+        </p>
+        <p
+          className={dropdownSection === 'help' ? 'active' : ''}
+          onClick={() => handleMenuClick('help', helpRef)}
+          ref={helpRef}
+        >
+          Help
+        </p>
 
-        {isDropdown && dropdownPosition && (
+        {dropdownSection?.length && dropdownPosition && (
           <div
-            className="dropdown-menu"
+            className="dropdown-container"
             style={{
               left: dropdownPosition.left + 'px',
               top: dropdownPosition.top + 'px',
             }}
             ref={dropdownRef}
           >
-            <ul>
-              <li>New Finder Window</li>
-              <li>New Folder</li>
-              <li>New Folder with Selection</li>
-              <li>New Smart Folder</li>
-              <li>New tab</li>
-              <li>Open</li>
-              <li>Open With</li>
-              <li>Print</li>
-              <li>Close Window</li>
-              <li>Get Info</li>
-              <li>Rename</li>
-              <li>Compress</li>
-              <li>Duplicate</li>
-              <li>Make Alias</li>
-              <li>Show Original</li>
-              <li>Add to Sidebar</li>
-              <li>Move to Trash</li>
-              <li>Eject</li>
-              <li>Find</li>
-              <li>Tags...</li>
-            </ul>
+            <Dropdown />
           </div>
         )}
       </div>
